43ed1585ea811fec73a1e0c8cb35214f77ae8215
Sabter Iqbal <sabterrazaqadri@gmail.com>
2024-12-22 02:13:42 +0500

    first commit

--- src/app/Pages/naat/page.tsx
+++ src/app/Pages/naat/page.tsx
@@ -8,20 +8,17 @@ const hamd: React.FC = () => {
     { id: 103, name: "رفعت عرش سے بھی رتبہ ہے بالا تیرا" },
     { id: 104, name: "میرے آقا کی ہے رحمت" },
     { id: 105, name: "محمد مصطفےٰ ﷺ صلِ علیٰ تشریف لے آئے" },
-    { id: 106, name: "" },
-    { id: 107, name: "" },
-    { id: 108, name: "" },
-    { id: 109, name: "" },
-    { id: 110, name: "" },
-    { id: 111, name: "" },
-    { id: 112, name: "" },
-    { id: 113, name: "" },
-    { id: 114, name: "" },
-    { id: 115, name: "" },
-    { id: 116, name: "" },
-    { id: 117, name: "" },
-    { id: 118, name: "" },
-    { id: 1, name: "" },
+    { id: 106, name: "تمہاری یاد سے دل کو ہیں راحتیں کیا کیا" },
+    { id: 107, name: "حسن یوسف اور طہ کا جلوہ اور ہے" },
+    { id: 108, name: "مصطفی کا خدا اور خود مصطفی" },
+    { id: 109, name: "میرے لفظوں میں شمس الضحیٰ" },
+    { id: 110, name: "مدينے بلانا ہمیں جان عالم" },
+    { id: 111, name: "میں کہ بے وقعت و بے مایا ہوں" },
+    { id: 112, name: "میں تو خود ان کےدر کا گدا ہوں" },
+    { id: 113, name: "اوج پانا میرے حضور کا ہے" },
+    { id: 114, name: "تم نور ہو میں اک ذرا ہوں" },
+    { id: 115, name: "روک لیتی ہے آپ کی نسبت" },
+    
   ];
 
   return (
@@ -29,7 +26,7 @@ const hamd: React.FC = () => {
       <h1 className="text-2xl font-bold mb-4 text-center text-shadow-blueGlow text-blue-600">نعت کی فہرست</h1>
       <ul className="space-y-2">
         {songs.map((song) => (
-          <li key={song.id} className="bg-white p-4 rounded-lg md:text-2xl text-center shadow-md font-semibold border ">
+          <li key={song.id} className="bg-white p-4 rounded-lg md:text-2xl text-lg text-center shadow-md font-semibold border ">
             <Link href={`/pages/lyrics/${song.id}`}>
              <h3 className="text-background border-b-2">{song.name}</h3>
             </Link>
